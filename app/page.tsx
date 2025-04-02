834d7b17710639029e1f72f8667b1ffc7b8aa681
brayanriuge <ringebrayandev.com>
2025-04-02 23:17:27 +0300

    designing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="relative bg-black-100 overflow-hidden mx-auto px-5 sm:px-10 flex justify-between items-center flex-col ">
+    <div className="relative  overflow-hidden mx-auto px-5 sm:px-10 flex justify-between items-center flex-col bg-black-100">
       <div className="max-w-7xl w-full">
         <Hero />
       </div>
