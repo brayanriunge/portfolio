e3d662827643976f5d65bfa1cddc336c20001d90
brayanriuge <ringebrayandev.com>
2025-04-04 23:21:59 +0300

    adding grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,10 +26,7 @@ const Hero = () => {
           )}
         />
         {/* Radial gradient for the container to give a faded look */}
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-        <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-          Backgrounds
-        </p>
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
       </div>
     </div>
   );
