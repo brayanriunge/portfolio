aab4ad4115066df238c57ac4e1c1d5be20032815
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-04-08 21:33:37 +0300

    adding content to hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
