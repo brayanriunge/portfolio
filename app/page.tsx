9dd44cd9cfad5b4419d4e6b1a84c9d935909a3b2
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-04-09 22:58:06 +0300

    floating navbar and its props

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="relative  overflow-hidden mx-auto px-5 sm:px-10 flex justify-between items-center flex-col bg-[#000319]">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
       </div>
     </div>
