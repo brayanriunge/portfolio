05b717af16b24d5d7feb182c6d65f1ede39c9714
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-05-26 17:11:21 +0300

    bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBG";
 import { GlobeDemo } from "./GridGlobe";
+import { div } from "motion/react-client";
 
 export const BentoGrid = ({
   className,
@@ -96,6 +97,21 @@ export const BentoGridItem = ({
               {title}
             </div>
             {/* {id === 2 && <GlobeDemo />} */}
+
+            {id === 3 && (
+              <div className="flex gap-1 absolute lg:gap-5 w-fit  -right-3 lg:-right-2">
+                <div className="flex flex-col gap-3 lg:gap-8">
+                  {["React.js", "Next.js", "TypeScript"].map((item) => (
+                    <span
+                      key={item}
+                      className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
