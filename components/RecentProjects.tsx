e715b54ab092f21aaaeff9f96ba7a7ef75fa2312
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-06-29 19:49:03 +0300

    Projects component designing

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
 export const RecentProjects = () => {
   return (
@@ -15,12 +16,12 @@ export const RecentProjects = () => {
             key={id}
             className="lg:min-h-[32.5 rem] h-[25rem] flex justify-center items-center w-[80vw] sm:w-96"
           >
-            <PinContainer title={title} href={link}>
+            <PinContainer title={link} href={link}>
               <div className=" flex items-center justify-center relative overflow-hidden sm:w-96 w-[80vw] h-[20vh] lg:h-[30vh] mb-10">
                 <div className="relative w-full h-full lg:rounded-3xl bg-[#13162d] overflow-hidden">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                <img src="img" alt={title} className="z-10 absolute bottom-0" />
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
@@ -39,6 +40,12 @@ export const RecentProjects = () => {
                       <img src={icon} alt={icon} className="p-2" />
                     </div>
                   ))}
+                </div>
+                <div className="flex items-center justify-center ">
+                  <p className="flex text-sm lg:text-xl md:text-xs text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
             </PinContainer>
